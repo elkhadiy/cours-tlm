b87c26d350a142e0f01b237c6a779bda3ed7165e
Yassine El Khadiri <yassine.el-khadiri@ensimag.grenoble-inp.fr>
2015-11-19 19:54:17 +0100

    [TP2] Question 2 (corr)
    Finalement il faut vraiment mettre une plage d'addresse pour exposer des registres
    sur le bus.

--- TPs/squelette/tp2/defines.h
+++ TPs/squelette/tp2/defines.h
@@ -3,11 +3,14 @@
 
 #define SINGLE_REG 0x1
 
-#define MEM_BASE 0x10000000
-#define MEM_SIZE 0x15400
+#define MEM_BASE       0x10000000
+#define MEM_PDATA_SIZE 10*1024 // 10 Kio
+#define MEM_VBUF_BASE  MEM_BASE+MEM_PDATA_SIZE
+#define MEM_VBUF_SIZE  320*240 // 1o par pixel
+#define MEM_SIZE       0x15400
 
 #define LCDC_ADDR_BASE  0x0
-#define LCDC_ADDR_RANGE 0x3
+#define LCDC_ADDR_RANGE 0x8
 #define LCD_FREQ 1.0 / 25
 
 #define FG_RED     "\033[31m"
